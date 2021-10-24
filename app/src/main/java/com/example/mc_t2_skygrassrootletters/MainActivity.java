236bb9c1ffabf345b04f773a500f0a554e7a6a28
Muhammad Ahsan <muhammad.ahsan9776@gmail.com>
2021-10-24 23:31:56 +0500

    designed UI and added functionality

---
+++ app/src/main/java/com/example/mc_t2_skygrassrootletters/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.mc_t2_skygrassrootletters;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView letterStatus;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        letterStatus = (TextView) findViewById(R.id.textView);
+    }
+
+    public void updateStatus(View view) {
+        Button btn = (Button)view;
+        char ch = btn.getText().toString().charAt(0);
+        if(isRootLetter(ch)){
+            letterStatus.setText("Root Letter");
+        }else if(isSkyLetter(ch)){
+            letterStatus.setText("Sky Letter");
+        }else{
+            letterStatus.setText("Grass Letter");
+        }
+    }
+    public boolean isRootLetter(char ch)
+    {
+        if(ch=='g' || ch=='j'||ch=='p'||ch=='q'||ch=='y')
+            return true;
+        return false;
+    }
+    public boolean isSkyLetter(char ch)
+    {
+        if(ch=='b' || ch=='d'||ch=='f'||ch=='h'||ch=='l'||ch=='k'||ch=='t')
+            return true;
+        return false;
+    }
+}
